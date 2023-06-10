443d7b6240ab4f398fbf1ffd8438133a023a64fb
AMD <90222287+michojekunle@users.noreply.github.com>
2023-06-10 16:57:43 +0100

    Update Card.jsx

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -11,25 +11,25 @@ const Card = ({ _id, name, prompt, photo }) => {
         alt={prompt}
         className='w-full h-auto object-cover rounded-xl'
       />
-      <div className='group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md'>
+      <div className='group-hover:bottom-0 flex flex-col max-h-[94.5%] hidden absolute bottom-[-100%] transition-all duration-300 delay-150 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md'>
           <p className='text-white text-md overflow-y-auto prompt'>{prompt}</p>
           <div className='mt-5 flex justify-between items-center gap-2'>
             <div className='flex items-center gap-2'>
-              <div className='w-7 h-7 rounded-full object-cover bg-green-700 flex justify-center text-white font-bold text-xs'>{name[0]}</div>
-                <p className='text-white text-sm'>{name}</p>
+              <div className='w-7 h-7 rounded-full object-cover bg-green-700 flex justify-center items-center text-white font-bold text-xs'>{name[0]}</div>
+              <p className='text-white text-sm'>{name}</p>
             </div>
+            <button
+              type='button'
+              onClick={() => downloadImage(_id, photo)}
+              className='outline-none bg-transparent border-none'
+            >
+              <img
+                src={download}
+                alt="download"
+                className='w-6 h-6 object-contain invert'
+              />
+            </button>
           </div>
-          <button
-            type='button'
-            onClick={() => downloadImage(_id, photo)}
-            className='outline-none bg-transparent border-none'
-          >
-            <img
-              src={download}
-              alt="download"
-              className='w-6 h-6 object-contain invert'
-            />
-          </button>
       </div>
     </div>
   )
