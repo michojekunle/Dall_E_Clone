f48332cf9a96b9505c5a0281a51245b257588076
AMD <90222287+michojekunle@users.noreply.github.com>
2023-06-10 14:52:40 +0100

    Update connect.js

--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -1,8 +1,6 @@
 import mongoose from 'mongoose';
 
 const connectDB = (url) => {
-    mongoose.set('strictQuery', true);
-
     mongoose.connect(url)
         .then(() => console.log('MongoDB Connected'))
         .catch((err) => console.log(err))
