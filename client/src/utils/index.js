1a4fa152b8bd31065efcc77b29ae5478f3e29322
AMD <90222287+michojekunle@users.noreply.github.com>
2023-06-12 09:18:52 +0100

    Update index.js

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -13,5 +13,24 @@ export function getRandomPrompt(prompt) {
 }
 
 export async function downloadImage(_id, photo) {
-    FileSaver.saveAs(photo, `download-${_id}.jpg`);
+    base_image = new Image();
+    base_image.src = photo;
+    
+    var canvas = document.getElementById("MyCanvas");
+    context = canvas.getContext('2d');
+
+    // Draw image within
+    context.drawImage(base_image, 0,0);
+    
+    // Save the canvas
+    saveCanvas(canvas, _id);
+}
+
+function saveCanvas(x_canvas, _id){
+    x_canvas.toBlob(function(blob) {
+        saveAs(
+            blob
+            , `download-${_id}.png`
+        );
+    }, "image/png");
 }
