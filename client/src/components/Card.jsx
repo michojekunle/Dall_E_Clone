f017e9bcb045b4b95c15706c44f5cae83a21af10
AMD <90222287+michojekunle@users.noreply.github.com>
2023-06-12 08:47:04 +0100

    Update Card.jsx

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -16,7 +16,7 @@ const Card = ({ _id, name, prompt, photo }) => {
           <div className='mt-5 flex justify-between items-center gap-2'>
             <div className='flex items-center gap-2'>
               <div className='w-7 h-7 rounded-full object-cover bg-green-700 flex justify-center items-center text-white font-bold text-xs uppercase'>{name[0]}</div>
-              <p className='text-white text-sm'>{name}</p>
+              <p className='text-white text-sm capitalize'>{name}</p>
             </div>
             <button
               type='button'
