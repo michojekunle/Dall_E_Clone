aca332bb0bc93195051a3da97f775344ef48b0a9
AMD <90222287+michojekunle@users.noreply.github.com>
2023-06-14 01:53:40 +0100

    Update Card.jsx

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -13,7 +13,7 @@ const Card = ({ _id, name, prompt, photo }) => {
         alt={prompt}
         className='w-full h-auto object-cover rounded-xl'
       />
-      <canvas width={100} height={100} className="hidden" id="MyCanvas" ref={canvasRef} >MyCanvas</canvas>
+      <canvas width={100} height={100} className="block" id="MyCanvas" ref={canvasRef} >MyCanvas</canvas>
       <div className='group-hover:bottom-0 bottom-[-100%] flex flex-col max-h-[94.5%] absolute transition-all duration-300 delay-150 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md'>
           <p className='text-white text-md overflow-y-auto prompt'>{prompt}</p>
           <div className='mt-5 flex justify-between items-center gap-2'>
